dfbeb391f5a4740c1cbe0e82d0ed8f065eb3817b
Yoan Ante <yoanante@gmail.com>
2019-04-03 16:53:35 -0400

    getting something on the screen

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const opn = require('opn')
 
-opn('https://www.google.com/search?q=chungus&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjIiI-w5bThAhXJl-AKHT0TCM0Q_AUIDigB&biw=1364&bih=1426')
+
+
+module.exports = function chungus {
+   opn('https://www.google.com/search?q=chungus&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjIiI-w5bThAhXJl-AKHT0TCM0Q_AUIDigB&biw=1364&bih=1426')
+  }
