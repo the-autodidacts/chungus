cad396196655cfe1c7de2a618990a2516e02c726
Yoan Ante <yoanante@gmail.com>
2019-04-03 16:57:01 -0400

    parenthesis

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const opn = require('opn')
 
 
 
-module.exports = function chungus {
+module.exports = function chungus (){
    opn('https://www.google.com/search?q=chungus&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjIiI-w5bThAhXJl-AKHT0TCM0Q_AUIDigB&biw=1364&bih=1426')
   }
